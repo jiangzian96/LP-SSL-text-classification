e3677f98a5b9105c79cd820214426a3a0697ca8b
Zian Jiang <zj444@nyu.edu>
2020-03-15 22:03:04 -0400

    add parser

--- data-local/data_prep.py
+++ data-local/data_prep.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
+import argparse
 from utils import *
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -23,22 +24,29 @@ train_df = df[val_size:]
 
 train_texts, train_labels = np.array(train_df.v2), np.array(train_df.v1, dtype="int")
 val_texts, val_labels = np.array(val_df.v2), np.array(val_df.v1, dtype="int")
+all_labels = np.copy(train_labels)
 
 # make unlabeled
-labeled_texts, train_labels, unlabeled_texts, unlabeled_labels = create_unlabeled(train_texts, train_labels, num_labeled=250)
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--num_labeled", default=200, type=int, help="number of labeled example per class")
+args = parser.parse_args()
+NUM_LABELED = args.num_labeled
+labeled_texts, train_labels, unlabeled_texts, unlabeled_labels = create_unlabeled(train_texts, train_labels, num_labeled=NUM_LABELED)
 
 # string ---> tokens
 labeled_processed = tokenize_data(labeled_texts)
 unlabeled_processed = tokenize_data(unlabeled_texts)
 val_processed = tokenize_data(val_texts)
+all_processed = labeled_processed + unlabeled_processed
 
 # create vocab
-token2id, id2token = create_vocab(labeled_processed + unlabeled_processed, max_vocab=10000)
+token2id, id2token = create_vocab(all_processed, max_vocab=10000)
 
 # tokens --- > indices
 labeled_indices, train_labels = transform(labeled_processed, train_labels, token2id)
 unlabeled_indices, unlabeled_labels = transform(unlabeled_processed, unlabeled_labels, token2id)
 val_indices, val_labels = transform(val_processed, val_labels, token2id)
+all_indices, all_labels = transform(all_processed, all_labels, token2id)
 
 # create dataloaders
 BATCH_SIZE = 32
@@ -61,11 +69,18 @@ unlabeled_loader = torch.utils.data.DataLoader(dataset=unlabeled_dataset,
                                                batch_size=BATCH_SIZE,
                                                collate_fn=unlabeled_dataset.spam_collate_func,
                                                shuffle=False)
+
+all_dataset = SpamDataset(all_indices, all_labels, train_dataset.max_sent_length)
+all_loader = torch.utils.data.DataLoader(dataset=all_dataset,
+                                         batch_size=BATCH_SIZE,
+                                         collate_fn=all_dataset.spam_collate_func,
+                                         shuffle=False)
 print("Creating dataloaders done!")
 
 # save
-torch.save(unlabeled_loader, 'data-local/processed/unlabeled_loader.pth')
-torch.save(train_loader, 'data-local/processed/train_loader.pth')
-torch.save(val_loader, 'data-local/processed/val_loader.pth')
-with open('data-local/processed/token2id.pickle', 'wb') as handle:
+torch.save(unlabeled_loader, 'data-local/processed/unlabeled_loader_{}.pth'.format(NUM_LABELED))
+torch.save(train_loader, 'data-local/processed/train_loader_{}.pth'.format(NUM_LABELED))
+torch.save(val_loader, 'data-local/processed/val_loader_{}.pth'.format(NUM_LABELED))
+torch.save(all_loader, 'data-local/processed/all_loader_{}.pth'.format(NUM_LABELED))
+with open('data-local/processed/token2id_{}.pickle'.format(NUM_LABELED), 'wb') as handle:
   pickle.dump(token2id, handle, protocol=pickle.HIGHEST_PROTOCOL)
